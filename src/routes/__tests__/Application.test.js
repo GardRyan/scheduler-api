4ff174dbc07bed7116eb0a1c2461b4cdd0c31619
GardRyan <res.gardner@gmail.com>
2024-03-10 17:20:38 -0400

    recommit

--- src/routes/__tests__/Application.test.js
+++ src/routes/__tests__/Application.test.js
@@ -44,3 +44,53 @@ const day = getAllByTestId(container, "day").find(day =>
 );
 
 console.log(prettyDOM(day));
+
+it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  // 1. Render the Application.
+  const { container } = render(<Application />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  // 3. Click the "Delete" button on the booked appointment.
+  const appointment = getAllByTestId(container, "appointment").find(
+    appointment => queryByText(appointment, "Archie Cohen")
+  );
+
+  fireEvent.click(queryByAltText(appointment, "Delete"));
+
+  // 4. Check that the confirmation message is shown.
+  expect(
+    getByText(appointment, "Are you sure you would like to delete?")
+  ).toBeInTheDocument();
+
+  // 5. Click the "Confirm" button on the confirmation.
+  fireEvent.click(queryByText(appointment, "Confirm"));
+
+  // 6. Check that the element with the text "Deleting" is displayed.
+  expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+  // 7. Wait until the element with the "Add" button is displayed.
+  await waitForElement(() => getByAltText(appointment, "Add"));
+
+  // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+});
+
+  debug();
+
+
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {
+  // 1. Render the Application.
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  // 3. Click the "Edit" button on the booked appointment.
+  // 4. Check that the confirmation message is shown.
+  // 5. Click the "Confirm" button on the confirmation.
+  // 6. Check that the element with the text "Editing" is displayed.
+  // 7. Wait until the element with the "Add" button is displayed.
+  // 8. Check that the DayListItem with the text "Monday" also has the text "1 spots remaining".
+});
