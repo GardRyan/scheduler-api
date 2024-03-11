7ea88324f2c0c3fb0e6860ece06d76c38ec00e25
GardRyan <res.gardner@gmail.com>
2024-03-11 16:05:20 -0400

    recommit

---
+++ src/components/__tests__/Application.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { waitForElement } from "@testing-library/react";
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+});
